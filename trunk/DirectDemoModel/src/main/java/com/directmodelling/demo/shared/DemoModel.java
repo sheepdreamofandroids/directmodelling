858d17715a8c8ec8b07aa946d72fa524bb3fc744
guus@bloemsma.net <guus@bloemsma.net@62becc44-3d4c-0ee9-25c5-e85c5f5207ba>
2011-02-05 18:11:18 +0000

    Initial import.

---
+++ trunk/DirectDemoModel/src/main/java/com/directmodelling/demo/shared/DemoModel.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) Guus C. Bloemsma
+ */
+package com.directmodelling.demo.shared;
+
+import java.io.Serializable;
+
+import com.directmodelling.impl.DoubleVar;
+import com.directmodelling.impl.ListVar;
+import com.directmodelling.impl.ObjectVar;
+import com.directmodelling.impl.Variable;
+import com.directmodelling.properties.HasMaximum;
+import com.directmodelling.properties.HasMinimum;
+
+
+public class DemoModel implements IDemoModel, Serializable {
+
+	public static final class MyDoubleVar extends DoubleVar implements HasMaximum<Double>, HasMinimum<Double> {
+
+		@Override
+		public Double getMaximum() {
+			// TODO Auto-generated method stub
+			return 10d;
+		}
+
+		public void setMaximum(final Double max) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public Double getMinimum() {
+			// TODO Auto-generated method stub
+			return -10d;
+		}
+
+		public void setMinimum(final Double min) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	ObjectVar<String> var = new ObjectVar<String>();
+	DemoModel.MyDoubleVar doub = new MyDoubleVar();
+	ListVar<IPerson> persons = new ListVar<IPerson>();
+	{
+		doub.set(5.3);
+		var.set("initial");
+	}
+
+	@Override
+	public Variable<String> var() {
+		return var;
+	}
+
+	@Override
+	public DoubleVar doub() {
+		return doub;
+	}
+
+	public DoubleVar getDoubleVar() {
+		return doub;
+	}
+}
